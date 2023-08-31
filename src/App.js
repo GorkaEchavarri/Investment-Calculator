e7eed4cb84982829342e704dfbb850e555702be3
Gorka <127848931+GorkaEchavarri@users.noreply.github.com>
2023-08-31 11:26:01 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import logo from './assets/investment-calculator-logo.png';
+import Form from './components/Form'
+import Results from './components/Results'
+
+function App() {
+
+  const [userInput, setUserInput] = useState(null)
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+
+  if (userInput) {
+
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+
+      <Form onCalculate={calculateHandler}/>
+
+      {!userInput && <p style={{textAlign: 'center'}}>No investment calculated yet.</p>}
+      {userInput && <Results initialInvestment={userInput['current-savings']} data={yearlyData}/>}
+
+    </div>
+  );
+}
+
+export default App;
